402b02d21048435e3b9d4acece52e45d59a30313
jkrasik <jakubkrasnickii@gmail.com>
2018-10-10 00:12:28 +0200

    skills projects and contact

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
 
 const textPlace = document.querySelector(".text-change");
 const textAr = "Welcome to my site!".split("");
+const NAV = document.querySelector(".navigator");
+let whenNavChange = document.querySelector(".about-me").offsetTop;
+
+function changeNav() {
+  if(window.scrollY >= whenNavChange - NAV.offsetHeight){
+    NAV.classList.add("change-color");
+  }
+  else {
+    NAV.classList.remove("change-color");
+  }
+}
 
 function writeText(i) {
   if(textAr[i] == " "){
@@ -15,3 +26,4 @@ function writeText(i) {
 let i = 0;
 
 var xd = setInterval(function(){writeText(i); i++;}, 200);
+window.addEventListener('scroll', changeNav);
